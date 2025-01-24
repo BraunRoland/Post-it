72e71aadc888cb198440d06a72565574ce7e47af
Braun Roland <rolibraun94@gmail.com>
2025-01-24 12:45:46 +0100

    kesz

---
+++ Post-it/PostIt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post_it
+{
+    public class PostIt
+    {
+        private string szin;
+        private string tartalom;
+
+        public PostIt(string szin)
+        {
+            this.szin = szin;
+            this.tartalom = "";
+        }
+
+        public string Szin { get => szin;}
+        public string Tartalom { get => tartalom;}
+
+        public string HozzaAdas(string szoveg)
+        {
+            string alap = this.tartalom;
+            string modositott = alap + szoveg;
+            if (modositott.Length > 100)
+            {
+                Console.WriteLine("Túl hosszú a szoveg! adjon meg egy másikat!");
+                this.tartalom = alap;
+                return this.tartalom;
+            }
+            else
+            {
+                this.tartalom = modositott;
+                return this.tartalom;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"szín: {this.szin}  tartalom: {this.tartalom}";
+        }
+    }
+}
--- Post-it/Program.cs
+++ Post-it/Program.cs
@@ -10,6 +10,17 @@ namespace Post_it
     {
         static void Main(string[] args)
         {
+            PostIt p1 = new PostIt("Sárga");
+            PostIt p2 = new PostIt("Piros");
+            PostIt p3 = new PostIt("Zöld");
+
+            p1.HozzaAdas("Jónapot!");
+            Console.WriteLine(p2);
+            p2.HozzaAdas("Egyszer volt, hol nem volt, egy apró faluban élt egy öreg cipész, akit mindenki tisztelt mestersége miatt. Egy napon különös vendég érkezett hozzá, egy vándor, aki egy pár cipőt kért tőle, de nem akármilyet: olyat, amely képes elvezetni viselőjét az igazság földjére. A cipész, bár csodálkozott a kéréseken, elfogadta a kihívást, és napokon át dolgozott a műhelyében. Végül elkészítette a cipőt, mely ragyogott a napfényben, mintha varázslat szőtte volna. A vándor hálásan megköszönte, felhúzta a cipőt, és elindult. A faluban mindenki emlékezett a napra, amikor a cipész álomból valóságot alkotott, és a legenda örökre fennmaradt.");
+            p3.HozzaAdas("Alma");
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
         }
     }
 }
